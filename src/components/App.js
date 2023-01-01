1b90336063ce3a0da35970c5be74a55a6a24d3fc
IHOR <97735057+ihoretti@users.noreply.github.com>
2023-01-02 01:23:29 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
 import Section from './section/section';
 import FeedbackOptions from './feedbackOptions/FeedbackOptions';
 import Statistics from './statistics/statistics';
@@ -10,60 +10,67 @@ export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-};
 
-// goodIncrement = () => {
-// this.setState(prevState => ({
-//     good: prevState.good + 1,
-// }));
-// };
-// neutralIncrement = () => {
-// this.setState(prevState => ({
-//     neutral: prevState.neutral + 1,
-// }));
-// };
-// badIncrement = () => {
-// this.setState(prevState => ({
-//     bad: prevState.bad + 1,
-// }));
-// };
+  // goodIncrement = () => {
+  // this.setState(prevState => ({
+  //     good: prevState.good + 1,
+  // }));
+  // };
+  // neutralIncrement = () => {
+  // this.setState(prevState => ({
+  //     neutral: prevState.neutral + 1,
+  // }));
+  // };
+  // badIncrement = () => {
+  // this.setState(prevState => ({
+  //     bad: prevState.bad + 1,
+  // }));
+  // };
+  const countTotalFeedback = good + neutral + bad;
 
-const onLeaveFeedback = e => {
-  const name = e.target.name;
-  useState(prevState => ({
-    [name]: prevState[name] + 1,
-  }));
-};
+  const buttonChangeStatistics = e => {
+    const name = e.target;
+    if (name === `good`) {
+      setGood(prevState => prevState + 1);
+    }
+    if (name === `neutral`) {
+      setNeutral(prevState => prevState + 1);
+    }
+    if (name === `bad`) {
+      setBad(prevState => prevState + 1);
+    }
 
-const countTotalFeedback = good + neutral + bad;
+    const countPositiveFeedbackPercentage =
+      Number.parseInt((good / countTotalFeedback()) * 100) || 0;
 
-const countPositiveFeedbackPercentage =
-  Number.parseInt((good / countTotalFeedback()) * 100) || 0;
+    const buttonsName = ['good', 'neutral', 'bad'];
 
-const objKey = Object.keys(useState);
+    // const objKey = Object.keys(this.state);
 
-return (
-  <Section title={'statictics form'} subtitle={'Please leave feedback'}>
-    {/* <Buttons
+    return (
+      <Section title={'statictics form'} subtitle={'Please leave feedback'}>
+        {/* <Buttons
             goodIncrement={this.goodIncrement}
             neutralIncrement={this.neutralIncrement}
             badIncrement={this.badIncrement} /> */}
-    <FeedbackOptions
-      options={{ ...useState }}
-      onLeaveFeedback={onLeaveFeedback}
-    />
+        <FeedbackOptions
+          options={{ buttonsName }}
+          onLeaveFeedback={buttonChangeStatistics}
+        />
 
-    {countTotalFeedback() ? (
-      <Statistics
-        title="Statistics"
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        total={countTotalFeedback()}
-        percentage={countPositiveFeedbackPercentage()}
-      />
-    ) : (
-      <Notification message="There is no feedback" />
-    )}
-  </Section>
-);
+        {countTotalFeedback() ? (
+          <Statistics
+            title="Statistics"
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            percentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    );
+  };
+};
